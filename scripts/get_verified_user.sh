2960f426606683bdc8c6a16a604bfb0f7008f68b
Michael Schilling <michael.schilling@moveagency.com>
2024-10-11 16:18:50 +0200

    ci: tweaks

--- scripts/get_verified_user.sh
+++ scripts/get_verified_user.sh
@@ -5,13 +5,15 @@
     get_verified_user() {
         local user_access_token=$1
 
-        response=$(curl -s -H "Authorization: Bearer $user_access_token" "$DESKBIRD_API_URL/user")
-        if [[ $? -ne 0 ]]; then
-            echo "API call failed"
-            return 1
-        fi
+        # response=$(curl -s -H "Authorization: Bearer $user_access_token" "$DESKBIRD_API_URL/user")
+        # if [[ $? -ne 0 ]]; then
+        #     echo "API call failed"
+        #     return 1
+        # fi
 
-        if [[ $(echo "$response" | jq -r '.status') == "401" ]]; then
+        # if [[ $(echo "$response" | jq -r '.status') == "401" ]]; then
+        #     echo "Trying again"
+
             # Refresh token
             refresh_access_token "$user_access_token"
 
@@ -23,7 +25,7 @@
                 echo "API call failed after token refresh"
                 return 1
             fi
-        fi
+        # fi
 
         echo "$response" | jq '.'
     }
@@ -31,9 +33,11 @@
     refresh_access_token() {
         local user_access_token=$1
         # Implement the logic to refresh the access token here
+        source ./scripts/update_refresh-token.sh
         echo "Refreshing access token for $user_access_token"
     }
 
     # Example usage
+    echo "Getting verified user"
     user_access_token="$DESKBIRD_ACCESS_TOKEN"
     get_verified_user "$user_access_token"
