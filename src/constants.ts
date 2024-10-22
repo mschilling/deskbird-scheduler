75981657cfe8f7dea56c4b17613c13e8a25dcb91
Michael Schilling <michael.schilling@moveagency.com>
2024-10-22 13:50:52 +0200

    chore: small refactoring

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,8 @@
-export const API_BASE_URL = "https://api.deskbird.com/v1.1";
-export const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
-export const GOOGLE_API_URL = "https://www.googleapis.com/oauth2/v4/token";
+export const API_BASE_URL = 'https://api.deskbird.com/v1.1';
 
-export const DESK_58 = 476809;
-export const DESK_56 = 476804;
+export const GOOGLE_API_URL = 'https://www.googleapis.com/oauth2/v4/token';
+export const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
+
+export const DESKBIRD_RESOURCE_ID = process.env.DESKBIRD_RESOURCE_ID;
+export const DESKBIRD_ZONE_ITEM_ID = process.env.DESKBIRD_ZONE_ITEM_ID;
+export const DESKBIRD_WORKSPACE_ID = process.env.DESKBIRD_WORKSPACE_ID;
